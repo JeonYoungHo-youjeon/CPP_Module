0c8ee6173915ee07f6410613eabd49af1daec12a
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-08-12 21:25:39 +0900

    테스트 내용 수정

--- 03/ex01/main.cpp
+++ 03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 18:12:05 by youjeon           #+#    #+#             */
-/*   Updated: 2022/08/12 21:22:57 by youjeon          ###   ########.fr       */
+/*   Updated: 2022/08/12 21:25:29 by youjeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,12 @@ int	main(void)
 	ScavTrap b("B");
 
 	std::cout << std::endl;
+	
 	a.attack("B");
 	b.takeDamage(a.getDamege());
 	b.beRepaired(3);
 	std::cout << std::endl;
+
 	b.attack("A");
 	a.takeDamage(b.getDamege());
 	a.beRepaired(10);

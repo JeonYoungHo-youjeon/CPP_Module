37ff51a918cdecdfcbe70a1233419905dc8bdd32
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-08-17 23:53:14 +0900

    02/ex03 main 테스트 수정

--- 02/ex03/main.cpp
+++ 02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 01:42:40 by youjeon           #+#    #+#             */
-/*   Updated: 2022/08/05 02:10:13 by youjeon          ###   ########.fr       */
+/*   Updated: 2022/08/17 23:52:48 by youjeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,20 @@ int	main(void)
 	Point	b(2, 9);
 	Point	c(0, 5);
 	Point	p(3, 7);
+	Point	p2(4, 7);
 
 	std::cout << "Point a.x = " << a.getX() << " a.y = " << a.getY() << std::endl;
 	std::cout << "Point b.x = " << b.getX() << " b.y = " << b.getY() << std::endl;
 	std::cout << "Point c.x = " << c.getX() << " c.y = " << c.getY() << std::endl;
 	std::cout << "Point p.x = " << p.getX() << " p.y = " << p.getY() << std::endl;
+	std::cout << "Point p2.x = " << p2.getX() << " p2.y = " << p2.getY() << std::endl;
 	if (bsp(a, b, c, p))
 		std::cout << "p is in the triangle" << std::endl;
 	else
 		std::cout << "p is out of the triangle" << std::endl;
+	if (bsp(a, b, c, p2))
+		std::cout << "p2 is in the triangle" << std::endl;
+	else
+		std::cout << "p2 is out of the triangle" << std::endl;
 	return 0;
 }

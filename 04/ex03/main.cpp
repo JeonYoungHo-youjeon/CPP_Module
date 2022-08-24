605724a09c413e2dfda31decb795902a10ab88e0
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-08-24 14:56:31 +0900

    04/ex03 main 수정

--- 04/ex03/main.cpp
+++ 04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 22:15:46 by youjeon           #+#    #+#             */
-/*   Updated: 2022/08/23 22:16:24 by youjeon          ###   ########.fr       */
+/*   Updated: 2022/08/24 14:56:12 by youjeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,25 @@
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
+	ICharacter* me = new Character("me");
+	ICharacter* bob = new Character("bob");
+	AMateria* tmp_ice = NULL;
+	AMateria* tmp_cure = NULL;
+	
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
-	ICharacter* me = new Character("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	ICharacter* bob = new Character("bob");
+	tmp_ice = src->createMateria("ice");
+	me->equip(tmp_ice);
+	tmp_cure = src->createMateria("cure");
+	me->equip(tmp_cure);
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->unequip(0);
+	me->unequip(1);
+	delete tmp_ice;
+	tmp_ice = NULL;
+	delete tmp_cure;
+	tmp_cure = NULL;
 	delete bob;
 	delete me;
 	delete src;

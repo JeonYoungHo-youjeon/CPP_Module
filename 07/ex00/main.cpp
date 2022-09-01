24a867140578bb9e704357e0f6865419b5216c54
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-09-01 20:36:47 +0900

    07/ex00 main 수정

--- 07/ex00/main.cpp
+++ 07/ex00/main.cpp
@@ -6,26 +6,26 @@
 /*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 14:26:29 by youjeon           #+#    #+#             */
-/*   Updated: 2022/09/01 17:37:47 by youjeon          ###   ########.fr       */
+/*   Updated: 2022/09/01 20:36:26 by youjeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
-int main(int ac, char *av[])
-{
-	if (ac != 3)
-	{
-		std::cout << "argument count is not 2" << std::endl;
-		return (1);
-	}	
-	std::string a = av[1];
-	std::string b = av[2];
+int main( void ) {
+	int a = 2;
+	int b = 3;
 
-	std::cout << "now. a : " << a << ", b : " << b << std::endl;
-	std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
-	swap(a, b);
-	std::cout << "do swap. a : " << a << ", b : " << b << std::endl;
-	return (0);
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }

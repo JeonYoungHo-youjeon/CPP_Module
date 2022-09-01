7b3a5e48d2d9cfd3153271e71024fa6620c80c27
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-09-02 00:29:59 +0900

    07/ex02 done.

---
+++ 07/ex02/Array.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 22:21:12 by youjeon           #+#    #+#             */
+/*   Updated: 2022/09/02 00:21:41 by youjeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <stdexcept>
+template <typename T>
+class Array {
+ private:
+	std::size_t len;
+	T *array;
+ public:
+	Array(void);
+	Array(std::size_t n);
+	Array(const Array& obj);
+	Array& operator=(const Array& obj);
+	~Array(void);
+	std::size_t size(void) const;
+	T& operator[] (std::size_t i);
+	const T& operator[] (std::size_t i) const;
+};
+
+# include "Array.tpp"
+
+#endif

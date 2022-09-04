af7b8056946e05b605f8164777a941c552ea1a3a
JeonYoungHo-youjeon <youjeon@student.42seoul.kr>
2022-09-04 17:27:06 +0900

    08/ex01 done.

---
+++ 08/ex01/Span.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youjeon <youjeon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 15:48:15 by youjeon           #+#    #+#             */
+/*   Updated: 2022/09/04 17:24:56 by youjeon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <vector>
+# include <algorithm>
+# include <exception>
+# include <stdexcept>
+# include <iterator>
+
+
+class Span {
+ private:
+	std::size_t size;
+	std::vector<int> v;
+	Span(void);
+ public:
+	Span(const Span& obj);
+	Span& operator=(const Span& obj);
+	~Span(void);
+	Span(std::size_t i);
+	void addNumber(int value);
+	void addRange(std::vector<int>::iterator const &begin, std::vector<int>::iterator const &end);
+	std::size_t shortestSpan() const;
+	std::size_t longestSpan() const;
+	std::size_t getSize() const;
+	std::vector<int> getVector() const;
+};
+
+#endif
